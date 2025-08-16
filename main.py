e7a036040caad7e26638651e088ace9b0c59ce68
isaacwide <isaac.aguilard@alumno.buap.mx>
2025-08-16 16:33:36 -0600

    add new post

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ except Exception as e:
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
